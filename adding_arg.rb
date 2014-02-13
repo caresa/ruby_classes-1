6418fd06d28a3153f716ad3524d99751644d774b
Caresa <unimportantfact@gmail.com>
2014-02-13 17:06:58 -0600

    correct file

---
+++ adding_arg.rb
@@ -0,0 +1,27 @@
+class AddingArg
+
+def need_smoothie(smoothie)
+	smoothie = smoothie
+	"olive, do you want a #{smoothie} smoothie?"
+end
+
+def tv(tv)
+	"olive likes #{tv}"
+end
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
+olive = AddingArg.new
+olive.need_smoothie("blueberry")
+olive.tv("sesame street")
